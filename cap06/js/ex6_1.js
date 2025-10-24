a1f29c525f4f644b2ed531645ef4eb24786a1783
Elvis Aron Presley Bail <elvisbail6@gmail.com>
2025-10-24 10:42:06 -0300

    Continuando estudos de JS com livro

--- cap06/js/ex6_1.js
+++ cap06/js/ex6_1.js
@@ -25,3 +25,47 @@ frm.addEventListener("submit", (e) => {
     frm.paciente.focus()
 
 })
+
+frm.bUrgencia.addEventListener("click", () => {
+    if(!frm.checkValidity()) {
+        alert("Informe o nome do do paciênte a ser atendido em caráter de urgência")
+        frm.paciente.focus()
+        return
+    }
+
+    const nome = frm.paciente.value
+    lista.unshift(nome)
+
+    let listaNomes = ''
+
+    lista.forEach((nomeLista , i) => {
+        listaNomes += `${i+1}* ${nomeLista} \n`
+    });
+
+    respLista.textContent = listaNomes
+    frm.paciente.value = ''
+    frm.paciente.focus()
+
+})
+
+frm.bAtender.addEventListener("click", () => {
+
+    if(lista.length == 0) {
+        alert(`Todos os paciêntes já foram atendidos !`)
+        frm.paciente.focus()
+        return
+    }
+
+    const pacienteAgora = lista.shift()
+
+    respNome.textContent = pacienteAgora
+
+    let listaNomes = ''
+
+    lista.forEach((nomeLista , i) => {
+        listaNomes += `${i+1}* ${nomeLista} \n`
+    });
+
+    respLista.textContent = listaNomes
+
+})
