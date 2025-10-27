a68639be9d15f709c30909138be82334bf52a889
Elvis Aron Presley Bail <elvisbail6@gmail.com>
2025-10-27 11:57:00 -0300

    Continuando estudos de JS com livro

---
+++ cap06/js/ex6_2.js
@@ -0,0 +1,43 @@
+const frm = document.querySelector("form")
+const erros = document.querySelector("#erros")
+const chances = document.querySelector("#chances")
+
+let numeroMisterioso = (Math.random() * 100).toFixed(0)
+
+let numeroFalado = []
+let contadorErros = 0
+let contarChances = 6
+
+frm.addEventListener("submit", (e) => {
+    e.preventDefault()
+
+    let numero = Number(frm.numero.value)
+
+    if(numeroFalado.includes(numero)) {
+        alert("Você já falou este número!")
+        return
+    }
+
+    numeroFalado.push(numero)
+
+    contadorErros++
+    contarChances--
+
+    if(contarChances == 0) {
+        alert("Você não adivinhou o Número. Fim de Jogo.")
+        alert(`O número era: ${numeroMisterioso}`)
+        location.reload()
+    }
+
+    if(numero == numeroMisterioso) {
+        alert(`Parabens! Você acertou ! O número era ${numeroMisterioso}`)
+        location.reload()
+    }
+
+    erros.textContent = contadorErros
+    chances.textContent = contarChances
+
+    frm.numero.value = ''
+    frm.numero.focus()
+
+})
